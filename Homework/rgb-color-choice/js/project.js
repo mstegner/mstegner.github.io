d93e7531a1796c8a723fd3926a14603cc29a207f
mstegner <maria.stegner@gmail.com>
2016-10-28 21:09:33 -0400

    Uploaded rgb-color-choice homework

---
+++ Homework/rgb-color-choice/js/project.js
@@ -0,0 +1,18 @@
+
+var button = document.getElementById('color-button');
+
+button.onclick = changeColor;
+
+
+function changeColor () {
+	var red = document.getElementById('red').value;
+	var green = document.getElementById('green').value;
+	var blue = document.getElementById('blue').value;
+
+var colorStr = 'rgb(' + red + ' ,' + green + ' ,' + blue + ')';
+
+document.getElementById('colorful-text').innerHTML = colorStr;
+
+document.getElementById('wrapper').style.background = colorStr;
+
+}
