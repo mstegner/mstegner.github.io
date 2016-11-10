f08d40c50665f7c02b10a8bd5f7e450a1ffc3a16
mstegner <maria.stegner@gmail.com>
2016-11-09 21:20:29 -0500

    uploaded list maker solution homework

---
+++ class-work/temp-converter/js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  
+  //on keypress event on #celsius, read celsius temp input 
+  //parseInt data to a number value, and calculate fahrenheit
+  //temp = celsius * 1.9 + 32
+  //write down fahrenheit value inside
+  //#fahrenheit input field
+
+$(#celsius).keypress('celsius') {
+
+  var temp = 
+  $(#celsius).parseInt();
+}
+
+}
+
+
+
+
+  
+})
