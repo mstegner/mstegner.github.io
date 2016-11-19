d2455bc5de05669c8fa4e6ee0f4fa281d83aac76
mstegner <maria.stegner@gmail.com>
2016-11-18 22:44:43 -0500

    updated html, css, and js files for final project

--- final-project/final_project/js/surf.js
+++ final-project/final_project/js/surf.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+//nav bar text color change on mouseenter/mouseleave
+
 $('.navbar a').mouseenter(function () {
 
 	$(this).css('color', '#f45409');
@@ -10,6 +12,8 @@ $('.navbar a').mouseleave(function() {
 	$(this).css('color', 'white');
 	
 });
+
+//link button color change on mouseenter/mouseleave
 
 $('.linkbutton').mouseenter(function () {
 
@@ -28,26 +32,57 @@ $('.linkbutton').mouseleave(function () {
 
 });
 
+//lessons blurb link color change on mouseenter/mouseleave
+
 $('.lessonsblurb a').mouseenter(function () {
 
 	$(this).css('color', '#f45409');
 
-})
+});
 
 
 $('.lessonsblurb a').mouseleave(function () {
 
 	$(this).css('color', 'black');
 
-})
+});
 
 
+//surfboard graphic color change on mouseenter/mouseleave
 
 
+$(".board").hover(function(){
+    $(this).attr("src", function(index, attr){
+        return attr.replace(".png", "_active.png");
+    });
+}, function(){
+    $(this).attr("src", function(index, attr){
+        return attr.replace("_active.png", ".png");
+    });
+});
+
+$('.board #gun').click(function(){
+	$('.boardicons').append('<p>Gun: This is the gun board.</p>');
 
 
+});
 
 
+// surfboard sizing calculator
+
+var heights = {
+	height1: [["/images/eight.png","/images/eightsix.png"],["/images/eight.png","/images/eightsix.png"],["/images/eightsix.png","/images/nine.png"],["/images/ninesix.png","/images/ten.png"],["/images/ninesix.png","/images/ten.png"]],
+	height2: [],
+	height3: [],
+	height4: []
+};
+function findBoards (height, weight){
+	var obj = heights[height][weight];
+	console.log(obj);
+	for (i=0; i < obj.length; i++){
+		$('body').prepend('<img src="' + obj[i] + '">');
+	}
+}
 
 
 
